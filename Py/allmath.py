1c2ed5a2706fbea0f3844be59a0562edb7143a28
DVP-F <carnx@duck.com>
2025-09-05 21:23:32 +0000

    documentation start

--- Py/allmath.py
+++ Py/allmath.py
@@ -1,8 +1,23 @@
+from __future__ import annotations # Treat type hints as strings/future types. this gets rid of 60+ errors per class
 import numpy as np
+from typing import Union
 
 # Custom type for storing 1024 byte unsigned integers
-class UInt1024:
-	"""This is a fixed-size unsigned 8192-bit integer type. It IMMEDIATELY takes 1GB of ram."""
+class UInt4096:
+	"""This is a fixed-size unsigned 8192-bit integer type. It IMMEDIATELY takes 1KB of ram."""
+
+	MAX = (1 << 1024) -1
+
+#! Avoiding subclassing int for the fine control applicable now :3
+#? (Im gonna have to create every magic method manually)
+#	def __new__(cls, value)->UInt1024:
+#		try:
+#			ivalue = int(value)
+#		except Exception as e:
+#			raise TypeError(f"Cannot convert from {type(value)} to UInt1024") from e
+#		if ivalue < 0 or ivalue > cls.MAX:
+#			raise ValueError(f"Value is out of range for UInt1024 (0...{cls.MAX})")
+#		return super().__new__(cls,value)
 
 	def __init__(self, value: int = 0):
 		self.chunks = np.zeros(128, dtype=np.uint64)
@@ -25,8 +40,8 @@ class UInt1024:
 	def __repr__(self):
 		return f"UInt1024({self._to_int()})"
 
-	def __add__(self, other: UInt1024)->UInt1024:
-		res = UInt1024()
+	def __add__(self, other: UInt4096)->UInt4096:
+		res = UInt4096()
 		carry = 0
 		for i in range(128):
 			total = int(self.chunks[i]) + int(other.chunks[i]) + carry
@@ -35,7 +50,7 @@ class UInt1024:
 		if carry != 0: raise OverflowError
 		return res
 
-	def __iadd__(self, other: UInt1024)->UInt1024:
+	def __iadd__(self, other: UInt4096)->UInt4096:
 		carry = 0
 		for i in range(128):
 			total = int(self.chunks[i]) + int(other.chunks[i]) + carry
@@ -44,8 +59,8 @@ class UInt1024:
 		if carry != 0: raise OverflowError
 		return self
 
-	def __mul__(self, other: UInt1024)->UInt1024:
-		res = UInt1024()
+	def __mul__(self, other: UInt4096)->UInt4096:
+		res = UInt4096()
 		carry = 0
 		for i in range(128):
 			total = int(self.chunks[i]) * int(other.chunks[i]) + carry
@@ -54,7 +69,7 @@ class UInt1024:
 		if carry != 0: raise OverflowError
 		return res
 
-	def __imul__(self, other: UInt1024)->UInt1024:
+	def __imul__(self, other: UInt4096)->UInt4096:
 		carry = 0
 		for i in range(128):
 			total = int(self.chunks[i]) * int(other.chunks[i]) + carry
@@ -63,131 +78,149 @@ class UInt1024:
 		if carry != 0: raise OverflowError
 		return self
 
-	def __sub__(self, other: UInt1024)->UInt1024:
-		res = UInt1024()
+	def __sub__(self, other: UInt4096)->UInt4096:
+		res = UInt4096()
 		for i in range(128):
 			total = int(self.chunks[i]) - int(other.chunks[i])
 			res.chunks[i] = total & 0xFFFFFFFFFFFFFFFF
 		return res
 
-	def __isub__(self, other: UInt1024)->UInt1024:
+	def __isub__(self, other: UInt4096)->UInt4096:
 		for i in range(128):
 			total = int(self.chunks[i]) - int(other.chunks[i])
 			self.chunks[i] = total & 0xFFFFFFFFFFFFFFFF
 		return self
 
-	def __truediv__(self, other: UInt1024)->UInt1024:
+	def __truediv__(self, other: UInt4096)->UInt4096:
 		return self
 
-	def __itruediv__(self, other: UInt1024)->UInt1024:
+	def __itruediv__(self, other: UInt4096)->UInt4096:
 		return self
 
-	def __floordiv__(self, other: UInt1024)->UInt1024:\
+	def __floordiv__(self, other: UInt4096)->UInt4096:\
 		return self
 
-	def __ifloordiv__(self, other: UInt1024)->UInt1024:\
+	def __ifloordiv__(self, other: UInt4096)->UInt4096:\
 		return self
 
-	def __mod__(self, other: UInt1024)->UInt1024:
+	def __mod__(self, other: UInt4096)->UInt4096:
 		return self
 
-	def __imod__(self, other: UInt1024)->UInt1024:
+	def __imod__(self, other: UInt4096)->UInt4096:
 		return self
 
-	def __pow__(self, other: UInt1024)->UInt1024:
+	def __pow__(self, other: UInt4096)->UInt4096:
 		return self
 
-	def __ipow__(self, other: UInt1024)->UInt1024:
+	def __ipow__(self, other: UInt4096)->UInt4096:
 		return self
 
-	def __neg__(self)->UInt1024:
+	def __neg__(self)->UInt4096:
 		return self
 
-	def __pos__(self)->UInt1024:
+	def __pos__(self)->UInt4096:
 		return self
 
-	def __abs__(self)->UInt1024:
+	def __abs__(self)->UInt4096:
 		return self
 
-	def __and__(self)->UInt1024:
+	def __and__(self)->UInt4096:
 		return self
 
-	def __or__(self)->UInt1024:
+	def __or__(self)->UInt4096:
 		return self
 
-	def __xor__(self)->UInt1024:
+	def __xor__(self)->UInt4096:
 		return self
 
-	def __lshift__(self)->UInt1024:
+	def __lshift__(self)->UInt4096:
 		return self
 
-	def __rshift__(self)->UInt1024:
+	def __rshift__(self)->UInt4096:
 		return self
 
-	def __iand__(self)->UInt1024:
+	def __iand__(self)->UInt4096:
 		return self
 
-	def __ior__(self)->UInt1024:
+	def __ior__(self)->UInt4096:
 		return self
 
-	def __ixor__(self)->UInt1024:
+	def __ixor__(self)->UInt4096:
 		return self
 
-	def __ilshift__(self)->UInt1024:
+	def __ilshift__(self)->UInt4096:
 		return self
 
-	def __irshift__(self)->UInt1024:
+	def __irshift__(self)->UInt4096:
 		return self
 
-	def __eq__(self)->bool:
-		return self
+	def __eq__(self, other: UInt4096)->bool:
+		for i in range(128):
+			if int(self.chunks[i]) != int(other.chunks[i]):
+				return False
+		return True
 
-	def __ne__(self)->bool:
-		return self
+	def __ne__(self, other: UInt4096)->bool:
+		for i in range(128):
+			if int(self.chunks[i]) != int(other.chunks[i]):
+				return False
+		return True
 
-	def __lt__(self)->bool:
-		return self
+	def __lt__(self, other: UInt4096)->bool:
+		for i in range(128):
+			if int(self.chunks[i]) != int(other.chunks[i]):
+				return False
+		return True
 
-	def __le__(self)->bool:
-		return self
+	def __le__(self, other: UInt4096)->bool:
+		for i in range(128):
+			if int(self.chunks[i]) != int(other.chunks[i]):
+				return False
+		return True
 
-	def __gt__(self)->bool:
-		return self
+	def __gt__(self, other: UInt4096)->bool:
+		for i in range(128):
+			if int(self.chunks[i]) != int(other.chunks[i]):
+				return False
+		return True
 
-	def __ge__(self)->bool:
-		return self
+	def __ge__(self, other: UInt4096)->bool:
+		for i in range(128):
+			if int(self.chunks[i]) != int(other.chunks[i]):
+				return False
+		return True
 
-	def __new__(self)->UInt1024:
-		return self
-
-	def __del__(self)->UInt1024:
+	def __del__(self)->UInt4096:
 		return self
 
 	def __int__(self)->int:
-		return self
+		return self._to_int()
 
 	def __bool__(self)->bool:
-		return self
+		return True if self._to_int() != 0 else False
 
 	def __str__(self)->str:
-		return self
+		res = ''
+		for i in range(128):
+			res += str(int(self.chunks[i]))
+		return res
 
 	def __bytes__(self)->bytes:
-		return self
+		return self._to_int().to_bytes(128, 'little')
 
 	def __len__(self)->int:
-		return self
+		return len(str(self))
 
-	def __getitem__(self)->np.uint64:
-		return self
+	def __getitem__(self)-> Union[np.bool_, np.uint8, np.uint16, np.uint32, np.uint64]:
+		...
 
-	def __setitem__(self)->UInt1024:
+	def __setitem__(self)->UInt4096:
 		return self
 
 	def __contains__(self)->bool:
 		return self
 
-	def __copy__(self)->UInt1024:
+	def __copy__(self)->UInt4096:
 		return self
 
 	def __sizeof__(self)->int:
--- README.md
+++ README.md
@@ -2,3 +2,119 @@
 Literally all of math in one file  
 
 Note our usage of other libraries when applicable :3  
+
+## Python Implementation  
+
+---  
+
+### Types:  
+
+- 64 Bit
+  - UInt
+    - UInt4096
+    - UInt2048
+    - UInt1024
+    - UInt512
+    - UInt256
+    - UInt128
+  - Int
+    - Int4096
+    - Int2048
+    - Int1024
+    - Int512
+    - Int256
+    - Int128
+  - Float
+    - Float4096
+    - Float2048
+    - Float1024
+    - Float512
+    - Float256
+    - Float128
+- 32 Bit
+  - UInt
+    - UInt4096H
+    - UInt2048H
+    - UInt1024H
+    - UInt512H
+    - UInt256H
+    - UInt128H
+  - Int
+    - Int4096H
+    - Int2048H
+    - Int1024H
+    - Int512H
+    - Int256H
+    - Int128H
+  - Float
+    - Float4096H
+    - Float2048H
+    - Float1024H
+    - Float512H
+    - Float256H
+    - Float128H
+
+### Values  
+
+- Fixed size  
+  - np.ndarray within
+    - Custom dunder modules
+  - Initial value: 0
+  - Chunks of np.uint64, np.int32 etc.
+- Indexing 
+  - Index
+    - Uses a 32 bit value of any type, simplest defined in hex or bin
+    - Table of structure, as default Little Endian (LE) 
+      - Indexes depend on a declaration in Mode Denotion to define which one is applicable.
+
+      | Bit addresses | Use                       | Return size in bits | Return size in bytes | Expected Values |
+      |---------------|---------------------------|---------------------|----------------------|-----------------|
+      | 31            | Version flag              | -                   | -                    | 0 ... 1         |
+      | 30 ... 27     | Mode denotion             | -                   | -                    | 0 ... 11        |
+      | 26 ... 19     | Reserved                  | -                   | -                    | 0 ... 511       |
+      | 18 ... 16     | Flags                     | -                   | -                    | 0 ... 11        |
+      | 18            | Signed / Two's Complement | -                   | -                    | 0 ... 1         |
+      | 17            | Chunk Selection           | -                   | -                    | 0 ... 1         |
+      | 16            | Endianness                | -                   | -                    | 0 ... 1         |
+      | 15 ... 0      | Bit index                 | 1                   | 1/8                  | 0 ... 8191      |
+      | 14 ... 0      | Nybble index              | 4                   | 1/2                  | 0 ... 2047      |
+      | 13 ... 0      | Byte index                | 8                   | 1                    | 0 ... 1023      |
+      | 12 ... 0      | Word index                | 16                  | 2                    | 0 ... 511       |
+      | 11 ... 0      | DWord index               | 32                  | 4                    | 0 ... 255       |
+      | 10 ... 0      | Qword index               | 64                  | 8                    | 0 ... 127       |
+      | 9  ... 0      | UInt128 \| Int128 index   | 128                 | 16                   | 0 ... 63        |
+      | 8  ... 0      | UInt256 \| Int256 index   | 256                 | 32                   | 0 ... 31        |
+      | 7  ... 0      | UInt512 \| Int512 index   | 512                 | 64                   | 0 ... 15        |
+      | 6  ... 0      | UInt1024 \| Int1024 index | 1024                | 128                  | 0 ... 7         |
+      | 5  ... 0      | UInt2048 \| Int2048 index | 2048                | 256                  | 0 ... 3         |
+      | 4  ... 0      | UInt4096 \| Int4096 index | 4096                | 512                  | 0 ... 1         |
+      | 3  ... 0      | NotImplemented            | -                   | -                    | 0               |
+      | 2  ... 0      | NotImplemented            | -                   | -                    | 0               |
+      | 1  ... 0      | NotImplemented            | -                   | -                    | 0               |
+      | 0             | NotImplemented            | -                   | -                    | 0               |
+
+    - The Mode denotion bits say what kind of value youre indexing to, and thus what size value should be expected in LSBs
+      - Additionally this will influence the reserved bits used for the actual index values
+      - Also note bit 31 is always 0 for these version 1 values. In later versions this can be used for an alternate set of modes.
+    
+      | Value (bits 31...27) | Value in decimal | Type                | Reserved bits for index value |
+      |----------------------|------------------|---------------------|-------------------------------|
+      | 0b0000               | 0                | Bit                 | 15 ... 0                      |
+      | 0b0001               | 1                | Nybble              | 14 ... 0                      |
+      | 0b0010               | 2                | Byte                | 13 ... 0                      |
+      | 0b0011               | 3                | Word                | 12 ... 0                      |
+      | 0b0100               | 4                | DWord               | 11 ... 0                      |
+      | 0b0101               | 5                | Qword               | 10 ... 0                      |
+      | 0b0110               | 6                | UInt128 \| Int128   | 9  ... 0                      |
+      | 0b0111               | 7                | UInt256 \| Int256   | 8  ... 0                      |
+      | 0b1000               | 8                | UInt512 \| Int512   | 7  ... 0                      |
+      | 0b1001               | 9                | UInt1024 \| Int1024 | 6  ... 0                      |
+      | 0b1010               | 10               | UInt2048 \| Int2048 | 5  ... 0                      |
+      | 0b1011               | 11               | UInt4096 \| Int4096 | 4  ... 0                      |
+      | 0b1100...0b1111      | 12-15            | Reserved            | 0                             |
+  
+  - Slice
+  - Ellipses  
+- Logic
+  - Per now uses other types' logic for most thing like len()
+- ...  
