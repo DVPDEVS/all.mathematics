d6ae577293f7f6478ab34c90b507841757e1ccce
DVP-F <carnx@duck.com>
2025-09-06 21:03:52 +0000

    wormk

--- Py/allmath.py
+++ Py/allmath.py
@@ -2,6 +2,8 @@ from __future__ import annotations # Treat type hints as strings/future types. t
 import numpy as np
 from typing import Union
 
+from fontTools.ttLib.tables.otConverters import UInt8
+
 # Custom type for storing 1024 byte unsigned integers
 class UInt4096:
 	"""This is a fixed-size unsigned 8192-bit integer type. It IMMEDIATELY takes 1KB of ram."""
@@ -209,7 +211,7 @@ class UInt4096:
 		return self._to_int().to_bytes(128, 'little')
 
 	def __len__(self)->int:
-		return len(str(self))
+		return len(str(self))*64 if type(self) ==  else 0
 
 	def __getitem__(self)-> Union[np.bool_, np.uint8, np.uint16, np.uint32, np.uint64]:
 		...
@@ -227,5 +229,183 @@ class UInt4096:
 		return self
 
 # Custom type for storing 1024 byte floats
+class Float4096:
+	"""This is a fixed-size signed 8192-bit float type. It IMMEDIATELY takes 1KB of ram."""
+
+	MAX = (1 << 1024) -1
+
+	def __init__(self, value: int = 0):
+		self.sign = 1
+		self.mantissa = np.zeros(128, dtype=np.uint64)
+		self.exponent = np.int16(0)
+		self._from_float(value)
+
+	def _from_float(self, value: int = 0):
+		i=0
+		while value > 0 and i < 128:
+			self.mantissa[i] = value & 0xFFFFFFFFFFFFFFFF
+			value >>= 64
+			i += 1
+
+	def _to_int(self)->int:
+		res = 0
+		for i in reversed(range(128)):
+			res <<= 64
+			res |= self.mantissa[i]
+		return res
+
+	def __repr__(self):
+		return f"UInt1024({self._to_int()})"
+
+	def __add__(self, other: Float4096)->Float4096:
+		return res
+
+	def __iadd__(self, other: Float4096)->Float4096:
+		return self
+
+	def __mul__(self, other: Float4096)->Float4096:
+		return res
+
+	def __imul__(self, other: Float4096)->Float4096:
+		return self
+
+	def __sub__(self, other: Float4096)->Float4096:
+		return res
+
+	def __isub__(self, other: Float4096)->Float4096:
+		return self
+
+	def __truediv__(self, other: Float4096)->Float4096:
+		return self
+
+	def __itruediv__(self, other: Float4096)->Float4096:
+		return self
+
+	def __floordiv__(self, other: Float4096)->Float4096:\
+		return self
+
+	def __ifloordiv__(self, other: Float4096)->Float4096:\
+		return self
+
+	def __mod__(self, other: Float4096)->Float4096:
+		return self
+
+	def __imod__(self, other: Float4096)->Float4096:
+		return self
+
+	def __pow__(self, other: Float4096)->Float4096:
+		return self
+
+	def __ipow__(self, other: Float4096)->Float4096:
+		return self
+
+	def __neg__(self)->Float4096:
+		return self
+
+	def __pos__(self)->Float4096:
+		return self
+
+	def __abs__(self)->Float4096:
+		return self
+
+	def __and__(self)->Float4096:
+		return self
+
+	def __or__(self)->Float4096:
+		return self
+
+	def __xor__(self)->Float4096:
+		return self
+
+	def __lshift__(self)->Float4096:
+		return self
+
+	def __rshift__(self)->Float4096:
+		return self
+
+	def __iand__(self)->Float4096:
+		return self
+
+	def __ior__(self)->Float4096:
+		return self
+
+	def __ixor__(self)->Float4096:
+		return self
+
+	def __ilshift__(self)->Float4096:
+		return self
+
+	def __irshift__(self)->Float4096:
+		return self
+
+	def __eq__(self, other: Float4096)->bool:
+		return True
+
+	def __ne__(self, other: Float4096)->bool:
+		return True
+
+	def __lt__(self, other: Float4096)->bool:
+		return True
+
+	def __le__(self, other: Float4096)->bool:
+		return True
+
+	def __gt__(self, other: Float4096)->bool:
+		return True
+
+	def __ge__(self, other: Float4096)->bool:
+		return True
+
+	def __del__(self)->Float4096:
+		return self
+
+	def __int__(self)->int:
+		return self._to_int()
+
+	def __bool__(self)->bool:
+		...
+
+	def __str__(self)->str:
+		res = ''
+		for i in range(128):
+			...
+		return res
+
+	def __bytes__(self)->bytes:
+		return self
+
+	def __len__(self)->int:
+		return len(str(self))
+
+	def __getitem__(self)-> Union[np.bool_, np.uint8, np.uint16, np.uint32, np.uint64]:
+		...
+
+	def __setitem__(self)->Float4096:
+		return self
+
+	def __contains__(self)->bool:
+		return self
+
+	def __copy__(self)->Float4096:
+		return self
+
+	def __sizeof__(self)->int:
+		return self
 
 
+
+class MathF:
+
+
+	bit64types = (UInt4096, UInt2048, UInt1024, UInt512, UInt256, UInt128, Int4096, Int2048, Int1024, Int512, Int256, Int128,
+	Float4096, Float2048, Float1024, Float512, Float256, Float128)
+
+	bit32types = (UInt4096H, UInt2048H, UInt1024H, UInt512H, UInt256H, UInt128H, Int4096H, Int2048H, Int1024H, Int512H, Int256H, Int128H,
+	Float4096H, Float2048H, Float1024H, Float512H, Float256H, Float128H)
+
+	def frexp(bigint):
+		if isinstance(bigint, MathF.bit64types):
+			return len(bigint)*64
+		elif isinstance(bigint, MathF.bit32types):
+			return len(bigint)*32
+		else: raise TypeError(f"Unsuported type: {type(bigint)}")
