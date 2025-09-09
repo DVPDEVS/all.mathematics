9d885571473c70e31dcad8d00e3a83978fbfe6e4
DVP-F <carnx@duck.com>
2025-09-09 13:46:09 +0200

    indexgen helper uwu

--- Py/allmath.py
+++ Py/allmath.py
@@ -443,45 +443,31 @@ class MathF:
 
 	#? Custom types
 	bigUIntsUnion64 = Union[UInt8192, UInt4096, UInt2048, UInt1024, UInt512, UInt256, UInt128]
-
 	bigIntsUnion64 = Union[Int8192, Int4096, Int2048, Int1024, Int512, Int256, Int128]
-
 	bigFloatsUnion64 = Union[Float8192, Float4096, Float2048, Float1024, Float512, Float256, Float128]
-
 	bigUIntsUnion32 = Union[UInt8192H, UInt4096H, UInt2048H, UInt1024H, UInt512H, UInt256H, UInt128H]
-
 	bigIntsUnion32 = Union[Int8192H, Int4096H, Int2048H, Int1024H, Int512H, Int256H, Int128H]
-
 	bigFloatsUnion32 = Union[Float8192H, Float4096H, Float2048H, Float1024H, Float512H, Float256H, Float128H]
 
 	#? Native and numpy types
 	intsUnion64 = Union[int, np.int_, np.int8, np.int16, np.int32, np.int64, np.intc]
-
 	uintsUnion64 = Union[np.uint, np.uint8, np.uint16, np.uint32, np.uint64, np.uintc]
-
 	floatsUnion64 = Union[float, np.float16, np.float32, np.float64, np.float96, np.float128]
-
 	intsUnion32 = Union[np.int8, np.int16, np.int32, np.intc]
-
 	uintsUnion32 = Union[np.uint8, np.uint16, np.uint32, np.uintc]
-
 	floatsUnion32 = Union[np.float16, np.float32]
 
 	#! Unionized unions get flattened, so this does actually work.
 	allIntsUnion32 = Union[bigIntsUnion32, intsUnion32]
-
 	allIntsUnion64 = Union[bigIntsUnion64, intsUnion64]
-
 	allUIntsUnion32 = Union[bigUIntsUnion32, uintsUnion32]
-
 	allUIntsUnion64 = Union[bigUIntsUnion64, uintsUnion64]
-
+	allUIntsUnion = Union[allUIntsUnion32, allUIntsUnion64]
+	allIntsUnion = Union[allIntsUnion32, allIntsUnion64]
+	allIntUIntsUnion = Union[allIntsUnion, allUIntsUnion]
 	allFloatsUnion32 = Union[bigFloatsUnion32, floatsUnion32]
-
 	allFloatsUnion64 = Union[bigFloatsUnion64, floatsUnion64]
-
 	bit32Union = Union[bigIntsUnion32, bigUIntsUnion32, bigFloatsUnion32, intsUnion32, uintsUnion32, floatsUnion32]
-
 	bit32Union = Union[bigIntsUnion64, bigUIntsUnion64, bigFloatsUnion64, intsUnion64, uintsUnion64, floatsUnion64]
 
 
@@ -489,34 +475,24 @@ class MathF:
 
 	#? 64 Bit
 	bigUInttypes64 = (UInt8192, UInt4096, UInt2048, UInt1024, UInt512, UInt256, UInt128)
-
 	uinttypes64 = (np.uint, np.uint16, np.uint32, np.uint64)
-
 	bigInttypes64 = (Int8192, Int4096, Int2048, Int1024, Int512, Int256, Int128)
-
 	inttypes64 = (np.int_, np.int8, np.int16, np.int32, np.int64, int)
-
 	bigFloattypes64 = (Float8192, Float4096, Float2048, Float1024, Float512, Float256, Float128)
-
 	floattypes64 = (np.float16, np.float32, np.float64)
 
 	#? 32 Bit
 	bigUInttypes32 = (UInt8192H, UInt4096H, UInt2048H, UInt1024H, UInt512H, UInt256H, UInt128H)
-
 	uinttypes32 = (np.uint8, np.uint16, np.uint32, np.uintc)
-
 	bigInttypes32 = (Int8192H, Int4096H, Int2048H, Int1024H, Int512H, Int256H, Int128H)
-
 	inttypes32 = (np.int8, np.int16, np.int32, np.intc)
-
 	bigFloattypes32 = (Float8192H, Float4096H, Float2048H, Float1024H, Float512H, Float256H, Float128H)
-
 	floattypes32 = (np.float16, np.float32, np.float64)
 
 
 	def frexp(bigint: MathF.bigIntsUnion64|MathF.bigIntsUnion32)->tuple[np.ndarray[int, np.uint64|np.uint32], int]:
 		"""Convert a bigint type (this library's int and uint types) to a floating point type. Returns the mantissa and exponent, not an object."""
-		bitcount = len(bigint)
+		bitcount = len(bigint) #? ngl i barely know what this does
 		if bitcount == 0:
 			return bigint, 0
 		chunk_size = bigint.chunks.itemsize * 8
@@ -525,4 +501,69 @@ class MathF:
 		normalized = bigint << shift
 		exponent = bitcount
 		return normalized, exponent
+
+	indexmodes = {
+		'bit'		:0b00000,
+		'nybble'	:0b00001,
+		'byte'		:0b00010,
+		'word'		:0b00011,
+		'dword'		:0b00100,
+		'qword'		:0b00101,
+		'uint128'	:0b00110,
+		'uint256'	:0b00111,
+		'uint512'	:0b01000,
+		'uint1024'	:0b01001,
+		'uint2048'	:0b01010,
+		'uint4096'	:0b01011,
+		#! and more is coming in later versions
+	}
+
+	def indexgen(mode: MathF.allUIntsUnion|int = 2, indexvalue: MathF.allIntUIntsUnion = 0, *, 
+			#! btw best for memory reasons to use the smalles uints you can, so here i'd pass np.uint8 for mode
+			signed: bool = False, 
+			littleEndian: bool = True,
+			chunkselect: bool|None = None, 
+			version2: bool = False) -> np.uint32:
+		if isinstance(mode, int):
+			mode = abs(mode) # we only use positive values for mode :3
+		index = 0
+		index |= ((mode & 0xF) << 27) # bit magic sets bits 30-27 to mode constrained to a value of 0...15
+		#? This is an in-place bitwise or, a bitwise and, and a bitwise leftshift
+		index |= ((signed & 0b1) << 18)
+		if chunkselect != None:
+			index |= ((chunkselect & 0b1) << 17)
+		index |= ((littleEndian & 0b1) << 16)
+		mask = 0 # build a mask for reserving bits in index value area/mask out indexvalue bits
+		for _ in range(mode):
+			mask >>= 1 # leftshift once
+			mask |= 0x8000 # this ors in one high bit (0b1000...)
+		index |= (indexvalue & ~mask) # mask out ignored bits, just in case
+		""" 
+		An example of an index which may be constructed with this:
+		mode = 3 [word]
+		signed = True [Negative value]
+		indexvalue = 391
+		==
+		0b 0001 1000 0000 0101 0000 0001 1000 0111
+		Bit 31 is locked as this is version 1								(0)
+		Bits 30-27 are the mode value clamped to 4 bits						(001 1)
+		Bits 26-19 are reserved 											0s
+		Bit 18 is the sign flag, indicating that this is a negative value 	(1)
+		Bit 17 is the chunk selection flag, set to 							0
+		Bit 16 is the endian indicator, set to little 						(1)
+		Bits 15-13 were masked out from the indexvalue 						(0s)
+		Bits 12-0 are our index 											(0 0001 1000 0111) (391)
+		//
+		Another example:
+		mode = 0
+		signed = False
+		indexvalue = 6753
+		==
+		0b 0000 0000 0000 0001 0001 1010 0110 0001
+		even then, in version one, the index is limited to certain max values. In this case, none of the
+		first 3 digits in the index matter, as the highest value accepted is 8191, while 16 bits can
+		supply 65535, and 13 bits = 0x1fff = 0b 0001 1111 1111 1111 = 8191
+		"""
+		return np.uint32(index)
+
 
